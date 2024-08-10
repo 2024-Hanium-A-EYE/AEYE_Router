d5449ca94f287aa314dd8f0e00dab8baeb87615c
EricoFetcher <yoonchul005@gmail.com>
2024-08-11 00:41:41 +0900

    added MW DataBase

--- AEYE_Router/mw/views/models.py
+++ AEYE_Router/mw/views/models.py
@@ -10,3 +10,13 @@ class aeye_inference_models (models.Model):
 
 class aeye_image_models(models.Model):
     image = models.ImageField(upload_to='images/')
+
+
+class aeye_database__models (models.Model):
+    whoami       = models.CharField(max_length=40)
+    message      = models.CharField(max_length=40)
+    operation   = models.CharField(max_length=40)
+    request_data = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
